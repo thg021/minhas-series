32140ac36d7618fbf8314ddb22dc78f0bde59caf
Thiago Silva <thiago.silva@hotmail.com>
2019-07-31 23:59:23 -0300

    Criação do componente HOME

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from './Header'
-
+import Home from './Home'
+import {
+  BrowserRouter as Router, 
+  Route
+} from 'react-router-dom'
 function App() {
-  return (
-    <div>
-      <Header />
-    </div>
+  return (   
+    <Router>
+      <div>
+          <Header />
+          <Route  path="/" component={Home} />
+      </div>
+    </Router>
   );
 }
 
